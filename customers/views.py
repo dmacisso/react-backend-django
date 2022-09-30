4961b0ae90bbcf9810e90038217240f2b44c6280
dmacisso <dmacisso@gmail.com>
2022-09-30 07:44:05 -0400

    basic 404

--- customers/views.py
+++ customers/views.py
@@ -1,6 +1,6 @@
 import imp
 from customers.models import Customer
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from customers.serialzers import CustomerSerializer
 
 
@@ -9,3 +9,13 @@ def customers(request):
     data = Customer.objects.all()
     serializer = CustomerSerializer(data, many=True)
     return JsonResponse({'customers': serializer.data})
+
+
+def customer(request, id):
+    # invoke serializer and return to client
+    try:
+        data = Customer.objects.get(pk=id)
+    except Customer.DoesNotExist:
+        raise Http404("Customer does not exist")
+    serializer = CustomerSerializer(data)
+    return JsonResponse({'customer': serializer.data})
